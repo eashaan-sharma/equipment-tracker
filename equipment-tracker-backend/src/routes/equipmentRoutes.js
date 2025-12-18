2171a6a553796bf7daacc9f38d572a43596a884c
Eashaan Sharma <120644754+eashaan-sharma@users.noreply.github.com>
2025-12-18 20:29:50 +0530

    Added post (create)

--- equipment-tracker-backend/src/routes/equipmentRoutes.js
+++ equipment-tracker-backend/src/routes/equipmentRoutes.js
@@ -11,4 +11,28 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const { name, type, status, lastCleanedDate } = req.body;
+
+    // Basic validation
+    if (!name || !type || !status || !lastCleanedDate) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const equipment = new Equipment({
+        name,
+        type,
+        status,
+        lastCleanedDate
+    });
+
+
+    const savedEquipment = await equipment.save();
+    res.status(201).json(savedEquipment);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to add equipment" });
+  }
+});
+
 module.exports = router;
