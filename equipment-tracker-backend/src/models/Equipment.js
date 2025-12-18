26379d9a656d6100c55eaae81f411869bb5462e4
Eashaan Sharma <120644754+eashaan-sharma@users.noreply.github.com>
2025-12-18 19:20:32 +0530

    equipment schema(parameters)

---
+++ equipment-tracker-backend/src/models/Equipment.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const equipmentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    type: {
+      type: String,
+      enum: ["Machine", "Vessel", "Tank", "Mixer"],
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ["Active", "Inactive", "Under Maintenance"],
+      required: true
+    },
+    lastCleanedDate: {
+      type: Date,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Equipment", equipmentSchema);
